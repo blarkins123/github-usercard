a1b360adc5449187e2855367716d47dac4c2f5bb
Brianna Larkins <blarkins123@gmail.com>
2020-07-15 19:40:44 -0700

    steps 1 and 2 done

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,8 +1,12 @@
+import axios from 'axios';
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+
+const axiosPromise = axios.get("https://api.github.com/users/brianna123")
+console.log(axiosPromise)
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
